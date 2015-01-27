fa7e97c641bc9ae47be3faefa8aa99c0fd37d2b9
chenhb37 <chenhb37@gmail.com>
2015-01-27 17:12:38 +0800

    1，增加Instance数据的读入接口
    2，修改kernel中算法实现的问题，当新解不被接受时，没有被还原，继续从新解出发。 已修复。并将将循环的中的变量声明设为共享内存变量

--- InstanceDataReader.m
+++ InstanceDataReader.m
@@ -1,18 +1,27 @@
-function [desc,nodes,satellites,demands] = InstanceDataReader( fileName )
-%UNTITLED Summary of this function goes here
+function [desc,nodes,satellites,demands] = InstanceDataReader(fileName)
+%DATAREADER Summary of this function goes here
 %   Detailed explanation goes here
- [fileId,message] = fopen(fileName);
- header = textscan(fileId,'%d %d %d %d',1,'delimiter',',');
- nodeNum = header{1};
- satelliteNum = header{2};
- vehicleNum = header{3};
- vehicleCapacity = header{4};
- desc = [nodeNum,satelliteNum,vehicleNum,vehicleCapacity];
- temp = textscan(fileId,'%s',1);
- nodes = textscan(fileId,'%d %f %f',nodeNum,'delimiter',',');
- temp = textscan(fileId,'%s',1);
- demands = textscan(fileId,'%d %d',nodeNum,'delimiter',',');
- temp = textscan(fileId,'%s',1);
- satellites = textscan(fileId,'%f %f',satelliteNum,'delimiter',',');
+ [fileId] = fopen(fileName);
+ %[dimension satellites customers]
+ nodeInfo = cell2mat(textscan(fileId,'%*s : %d',3,'HeaderLines',3)); 
+ %[l1capacity l2capacity l1fleet l2fleet] 
+ fleetInfo= cell2mat(textscan(fileId,'%*s : %d',2,'HeaderLines',3));
+ temp = cell2mat(textscan(fileId,'%*s %d',2,'HeaderLines',1));
+ fleetInfo = [fleetInfo;temp];
+ %node sate
+ nodes = cell2mat(textscan(fileId,'%*s %f %f %f -1',nodeInfo(1)-nodeInfo(2)-1,'HeaderLines',2));
+ satellites = cell2mat(textscan(fileId,'%*s %f %f %*d -1',nodeInfo(2)));
+ depot = cell2mat(textscan(fileId,'%*s %f %f %f -1',1));
+ nodes = [depot;nodes];
+ demands =nodes(1:nodeInfo(1)-nodeInfo(2),3);
+ demands(1) =0;
+ nodes =nodes(1:nodeInfo(1)-nodeInfo(2),1:2);
+ 
+%  header = textscan(fileId,'%d %d %d %d',1,'delimiter',',');
+%  nodeNum = header{1};
+%  satelliteNum = header{2};
+%  vehicleNum = header{3};
+%  vehicleCapacity = header{4};
+%  desc = [nodeNum,satelliteNum,vehicleNum,vehicleCapacity];
+ desc = [nodeInfo;fleetInfo];
 end
-
