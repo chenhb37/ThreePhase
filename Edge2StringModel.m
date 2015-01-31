6ab0542a46ef3a007acc6645f6b62171ca481049
chenhb37 <chenhb37@gmail.com>
2015-01-31 18:45:39 +0800

    增加小数据运行结果 smallData sheet in twoPhaseResult.xls
    修正边字符串模型转换 Edge2StringModel

--- Edge2StringModel.m
+++ Edge2StringModel.m
@@ -1,11 +1,12 @@
-function sm = Edge2StringModel(x,customerNum)
+function sm = Edge2StringModel(x,customerNum,demands)
   sm =[];
   x = int32(x);
   nodeNum = sqrt(size(x,1)/2);
   depotNum = nodeNum - customerNum;
   counter = 1;
   for i = 1:depotNum
-      for j = depotNum+1:nodeNum
+      j = depotNum+1;
+      while j<=nodeNum
           if x((i-1)*nodeNum+j) > 0
               %·¢ÏÖÂ·¾¶
               sm(counter) = i;
@@ -13,20 +14,30 @@ function sm = Edge2StringModel(x,customerNum)
               sm(counter) = j;
               counter = counter+1;
               x((i-1)*nodeNum+j) = x((i-1)*nodeNum+j)-1;
-              
+              prepre = i;
               pre = j;
               after = 1;
               while pre~= i
-                  while x((pre-1)*nodeNum+after) < 0.0000001
+                  while after<=nodeNum &&(x((pre-1)*nodeNum+after) < 0.0000001 || x(nodeNum^2+(pre-1)*nodeNum+after)<=0)
                       after = after + 1;
+                  end
+                  if after > nodeNum
+                      after = 1;
+                      while x((pre-1)*nodeNum+after) < 0.0000001
+                          after =after+1;
+                      end
                   end
                   sm(counter) = after;
                   counter = counter+1;
                   x((pre-1)*nodeNum+after) = x((pre-1)*nodeNum+after)-1;
+                  demands(pre) = 0;
+                  prepre = pre;
                   pre = after;
                   after = 1;
               end
+              j = j-1;
           end
+          j = j+1;
       end
   end
 end
